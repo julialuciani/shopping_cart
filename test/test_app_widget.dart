ce6bd07e870d7436af66439aa9d4fea0bd36e7e5
julialuciani <julia.luciani@warren.com.br>
2022-11-28 16:41:57 -0300

    resolving requested actions

--- test/test_app_widget.dart
+++ test/test_app_widget.dart
@@ -2,18 +2,11 @@ import 'package:flutter/material.dart';
 
 class TestAppWidget extends StatelessWidget {
   final Widget body;
-  final PreferredSizeWidget? appBar;
 
-  const TestAppWidget({Key? key, required this.body, this.appBar})
-      : super(key: key);
+  const TestAppWidget({Key? key, required this.body}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar,
-      body: Center(
-        child: body,
-      ),
-    );
+    return body;
   }
 }
